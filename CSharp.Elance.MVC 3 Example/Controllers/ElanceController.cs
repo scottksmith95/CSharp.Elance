e69ae99733c0ae311c5e0de95f486436f0f7ac41
Scott Smith <scottksmith95@gmail.com>
2012-12-08 11:03:47 -0800

    initial commit

---
+++ CSharp.Elance.MVC 3 Example/Controllers/ElanceController.cs
@@ -0,0 +1,53 @@
+﻿using System.Web.Mvc;
+using CSharp.Elance.Api.Interfaces;
+using CSharp.Elance.Connect;
+using Spring.Json;
+using Spring.Social.OAuth2;
+
+namespace CSharp.Elance.MVC_3_Example.Controllers
+{
+    public class ElanceController : Controller
+    {
+        // Configure the Callback URL
+        private const string CallbackUrl = "http://localhost:55002/Elance/Callback";
+
+        // Set your consumer client id (key) and secret
+        private const string ElanceApiClientId = "ENTER YOUR CLIENT ID HERE";
+        private const string ElanceApiSecret = "ENTER YOUR SECRET HERE";
+
+        readonly IOAuth2ServiceProvider<IElance> _elanceProvider = new ElanceServiceProvider(ElanceApiClientId, ElanceApiSecret);
+
+        public ActionResult Index()
+        {
+            var accessGrant = Session["AccessGrant"] as AccessGrant;
+            if (accessGrant != null)
+            {
+                //Until explict Elance endpoints have API bindings, the following
+                //method can be employed to consume Elance endpoints
+                var elanceClient = _elanceProvider.GetApi(accessGrant.AccessToken);
+                var result = elanceClient.RestOperations.GetForObject<JsonValue>("http://api.elance.com/api2/profiles/my?access_token=" + accessGrant.AccessToken);
+
+                ViewBag.AccessToken = accessGrant.AccessToken;
+                ViewBag.ResultText = result.ToString();
+
+                return View();
+            }
+
+            var parameters = new OAuth2Parameters
+            {
+                RedirectUrl = CallbackUrl,
+                Scope = "basicInfo"
+            };
+            return Redirect(_elanceProvider.OAuthOperations.BuildAuthorizeUrl(GrantType.AuthorizationCode, parameters));
+        }
+
+        public ActionResult Callback(string code)
+        {
+            AccessGrant accessGrant = _elanceProvider.OAuthOperations.ExchangeForAccessAsync(code, CallbackUrl, null).Result;
+
+            Session["AccessGrant"] = accessGrant;
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CSharp.Elance/Connect/BehanceServiceProvider.cs
@@ -0,0 +1,65 @@
+﻿#region License
+
+/*
+ * Copyright 2002-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#endregion
+
+using System;
+using CSharp.Elance.Api.Impl;
+using CSharp.Elance.Api.Interfaces;
+using Spring.Social.OAuth2;
+
+namespace CSharp.Elance.Connect
+{
+    /// <summary>
+    /// Elance <see cref="IServiceProvider"/> implementation.
+    /// </summary>
+    /// <author>Scott Smith</author>
+    public class ElanceServiceProvider : AbstractOAuth2ServiceProvider<IElance>
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="ElanceServiceProvider"/>.
+        /// </summary>
+        /// <param name="clientId">The application's API client id.</param>
+        /// <param name="clientSecret">The application's API client secret.</param>
+        public ElanceServiceProvider(String clientId, String clientSecret)
+            : base(new OAuth2Template(clientId, clientSecret,
+                "https://api.elance.com/api2/oauth/authorize",
+                "https://api.elance.com/api2/oauth/token"))
+        {
+        }
+
+        /// <summary>
+        /// Returns an API interface allowing the client application to access unprotected resources.
+        /// </summary>
+        /// <returns>A binding to the service provider's API.</returns>
+        public IElance GetAPi()
+        {
+            return new ElanceTemplate();
+        }
+
+        /// <summary>
+        /// Returns an API interface allowing the client application to access protected resources on behalf of a user.
+        /// </summary>
+        /// <param name="accessToken">The API access token.</param>
+        /// <returns>A binding to the service provider's API.</returns>
+        public override IElance GetApi(string accessToken)
+        {
+            return new ElanceTemplate(accessToken);
+        }
+    }
+}
